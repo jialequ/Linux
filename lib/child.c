7ca475a0de2e8e38eb8864f70dc8a5ac89d1e868
qujiale <qjl000312@163.com>
2019-10-10 22:56:34 +0800

    动态库静态库的使用

---
+++ lib/child.c
@@ -0,0 +1,13 @@
+/**********************************************************************
+ *    > File Name    : child.c
+ *    > Author       : qujiale
+ *    > Created Time : 2019年09月27日 星期五 14时49分32秒
+ *    > Description  :  
+ **********************************************************************/
+
+#include <stdio.h>
+#include "child.h"
+void print()
+{
+    printf("nihao\n");
+}
---
+++ lib/main.c
@@ -0,0 +1,13 @@
+/**********************************************************************
+ *    > File Name    : main.c
+ *    > Author       : qujiale
+ *    > Created Time : 2019年09月27日 星期五 14时50分04秒
+ *    > Description  :  
+ **********************************************************************/
+#include <stdio.h>
+#include "./include/child.h"
+int main()
+{
+    print();
+    return 0;
+}
